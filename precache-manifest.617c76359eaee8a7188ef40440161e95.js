af68757ccd1450401816214bd8f10e7e1e26fb3f
cbw29512 <divclass01@gmail.com>
2020-07-05 23:39:39 -0400

    Updates

---
+++ precache-manifest.617c76359eaee8a7188ef40440161e95.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeeea11a67ed7922d11a8f42cb575965",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "ff6eb5dfd7da79e8db50",
+    "url": "/weather_app/static/css/2.11e0aa6d.chunk.css"
+  },
+  {
+    "revision": "924d1ecc457b1c98f2da",
+    "url": "/weather_app/static/css/main.7dbc6e48.chunk.css"
+  },
+  {
+    "revision": "ff6eb5dfd7da79e8db50",
+    "url": "/weather_app/static/js/2.4f52188f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.4f52188f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924d1ecc457b1c98f2da",
+    "url": "/weather_app/static/js/main.9fde8064.chunk.js"
+  },
+  {
+    "revision": "f4a73882dead1478940d",
+    "url": "/weather_app/static/js/runtime-main.b94937d0.js"
+  },
+  {
+    "revision": "e47ffc6e9d6289da77eec42a3a869e25",
+    "url": "/weather_app/static/media/space-all-universe-cosmos.e47ffc6e.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather_app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather_app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather_app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather_app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather_app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
